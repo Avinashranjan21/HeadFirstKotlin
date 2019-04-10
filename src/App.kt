f42898bcddaef77c3d0759c27c1d69b48960b263
Avinash <Avinash.ranjan21@gmail.com>
2019-04-10 16:49:45 +0530

    In App.kt main function added.

---
+++ src/App.kt
@@ -0,0 +1,11 @@
+/**
+ * Whenever you write a Kotlin application, you must add a function to it called main,
+ * which starts your application. When you run your code, the JVM looks for this function,
+ * and executes it.
+ *
+ * From Kotlin1.3, however, you can omit main’s parameters so that the function looks like this
+ * */
+
+fun main() {
+    print("Pow!")
+}
